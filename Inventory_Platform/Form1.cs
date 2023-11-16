4729bf1b48f06629b6a438e02d050fefc7abe159
Anu Thandickal-Nazir <AT1064@student.aru.ac.uk>
2023-11-16 23:10:45 +0000

    registration page updated

---
+++ Inventory_Platform/Form1.cs
@@ -0,0 +1,30 @@
+namespace Inventory_Platform
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Inventory_Platform/frmLogin.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Inventory_Platform
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label6 = new Label();
+            label5 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            checkbxShowPas = new CheckBox();
+            txtpassword = new TextBox();
+            label3 = new Label();
+            txtUsername = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Cursor = Cursors.Hand;
+            label6.Font = new Font("Comic Sans MS", 7.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.SteelBlue;
+            label6.Location = new Point(77, 431);
+            label6.Name = "label6";
+            label6.Size = new Size(130, 19);
+            label6.TabIndex = 23;
+            label6.Text = "Create An Account";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Comic Sans MS", 7.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.ActiveCaption;
+            label5.Location = new Point(54, 412);
+            label5.Name = "label5";
+            label5.Size = new Size(167, 19);
+            label5.TabIndex = 22;
+            label5.Text = "Don't Have An Account?";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Nirmala UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.SteelBlue;
+            button2.Location = new Point(28, 374);
+            button2.Name = "button2";
+            button2.Size = new Size(216, 35);
+            button2.TabIndex = 21;
+            button2.Text = "CLEAR";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SteelBlue;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Nirmala UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(28, 322);
+            button1.Name = "button1";
+            button1.Size = new Size(216, 35);
+            button1.TabIndex = 20;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // checkbxShowPas
+            // 
+            checkbxShowPas.AutoSize = true;
+            checkbxShowPas.Cursor = Cursors.Hand;
+            checkbxShowPas.FlatStyle = FlatStyle.Flat;
+            checkbxShowPas.Font = new Font("Comic Sans MS", 7.8F, FontStyle.Bold, GraphicsUnit.Point);
+            checkbxShowPas.ForeColor = SystemColors.ActiveCaption;
+            checkbxShowPas.Location = new Point(143, 284);
+            checkbxShowPas.Name = "checkbxShowPas";
+            checkbxShowPas.Size = new Size(122, 23);
+            checkbxShowPas.TabIndex = 19;
+            checkbxShowPas.Text = "Show Password";
+            checkbxShowPas.UseVisualStyleBackColor = true;
+            // 
+            // txtpassword
+            // 
+            txtpassword.BackColor = Color.FromArgb(232, 233, 231);
+            txtpassword.BorderStyle = BorderStyle.None;
+            txtpassword.Font = new Font("Comic Sans MS", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            txtpassword.Location = new Point(40, 240);
+            txtpassword.Multiline = true;
+            txtpassword.Name = "txtpassword";
+            txtpassword.Size = new Size(216, 28);
+            txtpassword.TabIndex = 16;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.ActiveCaption;
+            label3.Location = new Point(40, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 25);
+            label3.TabIndex = 15;
+            label3.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.BackColor = Color.FromArgb(232, 233, 231);
+            txtUsername.BorderStyle = BorderStyle.None;
+            txtUsername.Font = new Font("Comic Sans MS", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUsername.Location = new Point(40, 165);
+            txtUsername.Multiline = true;
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(216, 28);
+            txtUsername.TabIndex = 14;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ActiveCaption;
+            label2.Location = new Point(40, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 25);
+            label2.TabIndex = 13;
+            label2.Text = "Email Id";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.SteelBlue;
+            label1.Location = new Point(40, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 48);
+            label1.TabIndex = 12;
+            label1.Text = "WELCOME";
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(289, 489);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkbxShowPas);
+            Controls.Add(txtpassword);
+            Controls.Add(label3);
+            Controls.Add(txtUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Comic Sans MS", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = SystemColors.ActiveCaption;
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmLogin";
+            Load += frmLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label6;
+        private Label label5;
+        private Button button2;
+        private Button button1;
+        private CheckBox checkbxShowPas;
+        private TextBox txtpassword;
+        private Label label3;
+        private TextBox txtUsername;
+        private Label label2;
+        private Label label1;
+    }
+}
