d068815974fb49b1f1510d41f6fed1aab5e57df2
Anu Thandickal-Nazir <AT1064@student.aru.ac.uk>
2023-11-22 15:50:35 +0000

    database updated

--- Inventory_Platform/dashboard.Designer.cs
+++ Inventory_Platform/dashboard.Designer.cs
@@ -30,11 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
             panel1 = new Panel();
-            panel2 = new Panel();
-            pictureBox1 = new PictureBox();
-            label1 = new Label();
-            panel3 = new Panel();
             panel4 = new Panel();
+            panel3 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
@@ -43,6 +47,10 @@
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(64, 64, 64);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
             panel1.Controls.Add(panel4);
             panel1.Controls.Add(panel3);
             panel1.Controls.Add(panel2);
@@ -51,6 +59,22 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(277, 700);
             panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(0, 99);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(15, 601);
+            panel4.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Right;
+            panel3.Location = new Point(262, 99);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(15, 601);
+            panel3.TabIndex = 2;
             // 
             // panel2
             // 
@@ -62,16 +86,6 @@
             panel2.Size = new Size(277, 99);
             panel2.TabIndex = 1;
             panel2.Paint += panel2_Paint;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(23, 26);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(50, 50);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // label1
             // 
@@ -85,20 +99,76 @@
             label1.TabIndex = 1;
             label1.Text = "DASHBOARD";
             // 
-            // panel3
+            // pictureBox1
             // 
-            panel3.Dock = DockStyle.Right;
-            panel3.Location = new Point(262, 99);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(15, 601);
-            panel3.TabIndex = 2;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(23, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(50, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
-            // panel4
+            // button1
             // 
-            panel4.Location = new Point(77, 194);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(250, 125);
-            panel4.TabIndex = 3;
+            button1.Cursor = Cursors.Hand;
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(15, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(247, 60);
+            button1.TabIndex = 4;
+            button1.Text = "VENDOR";
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(15, 159);
+            button2.Name = "button2";
+            button2.Size = new Size(247, 60);
+            button2.TabIndex = 5;
+            button2.Text = "PRODUCT";
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.Dock = DockStyle.Bottom;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(15, 640);
+            button3.Name = "button3";
+            button3.Size = new Size(247, 60);
+            button3.TabIndex = 6;
+            button3.Text = "HELP";
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Cursor = Cursors.Hand;
+            button4.Dock = DockStyle.Bottom;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(15, 580);
+            button4.Name = "button4";
+            button4.Size = new Size(247, 60);
+            button4.TabIndex = 7;
+            button4.Text = "INFO";
+            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button4.UseVisualStyleBackColor = true;
             // 
             // dashboard
             // 
@@ -128,5 +198,9 @@
         private Label label1;
         private Panel panel3;
         private Panel panel4;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Button button4;
     }
 }
--- Inventory_Platform/dashboard.cs
+++ Inventory_Platform/dashboard.cs
@@ -21,5 +21,10 @@ namespace Inventory_Platform
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
